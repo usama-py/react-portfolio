3bd7f00af82141178af48c7045181e73825303eb
usama-py <mohd.usama984@gmail.com>
2023-02-10 19:38:35 +0530

    styled and added experience component

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import ReactDOM from 'react-dom';
 import App from './App'
+import ReactDOM from "react-dom/client";
 import './index.css'
-ReactDOM.render(<App/>, document.querySelector('#root'));
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<App />);
