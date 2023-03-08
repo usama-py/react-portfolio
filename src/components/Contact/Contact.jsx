c1f3d863d0120dd875c429ce0addad7644533a62
usama-py <mohd.usama984@gmail.com>
2023-03-08 19:32:13 +0530

    styled and added Contact section and options for contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,8 +1,44 @@
 import React from 'react'
 import './contact.css'
+import {MdOutlineEmail} from 'react-icons/md'
+import {BsInstagram} from 'react-icons/bs'
+import {BsWhatsapp} from 'react-icons/bs'
 const Contact = () => {
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>
+        Get In Touch
+      </h5>
+      <h2>Contact Me</h2>
+      <div className='container contact__container'>
+        <div className='contact__options'>
+          <article className="contact__option">
+            <MdOutlineEmail className='contact__option-icon'/>
+            <h4>Email</h4>
+            <h5>mohd.usama984@gmail.com</h5>
+            <a href="mailto:mohd.usama984@gmail.com" target="_blank">Send A Message</a>
+          </article>
+          <article className="contact__option">
+            <BsInstagram className='contact__option-icon'/>
+            <h4>Instagram</h4>
+            <h5>usama_984</h5>
+            <a href="https://www.instagram.com/usama_984" target="_blank">Send A Message</a>
+          </article>
+          <article className="contact__option">
+            <BsWhatsapp className='contact__option-icon'/>
+            <h4>WhatsApp</h4>
+            <h5>Message Me</h5>
+            <a href="https://api.whatsapp.com/send/?phone=8639654409&text&type=phone_number&app_absent=0" target="_blank">Send A Message</a>
+          </article>
+        </div>
+        <form action="">
+          <input type="text" name='name' placeholder='Your Full Name' required />
+          <input type="email" name='email' placeholder='Your Email' required />
+          <textarea name="message" rows="" placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
