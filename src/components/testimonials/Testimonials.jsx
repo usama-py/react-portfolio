6ebe73b7490393c32505ba83ef1feca1d187c605
usama-py <mohd.usama984@gmail.com>
2023-03-09 15:11:59 +0530

    fixed responsive problem for small and medium devices

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -34,7 +34,7 @@ const data = [
 const Testimonials = () => {
   return (
     <section id='testimonials'>
-      <br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+      {/* <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /> */}
       <h5>Review from clients</h5>
       <h2>Testimonials</h2>
       <Swiper className='container testimonials__container' 
